d1a471f465edef402f4ab8a246c0732822b24c1a
Seetha Veezhinathan <33585446+Seetha-Veezhinathan@users.noreply.github.com>
2018-12-02 16:05:29 -0500

    Update Select1.java

--- Select1.java
+++ Select1.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.io.IOException;
 
 /* SELECT1: Sort the array using Quicksort and pick the kth smallest element. This has average time 
-complexity of O(n log n) and worst-case time of O(n2). */
+complexity of O(n log n) and worst-case time of O(n2).*/
 
 public class Select1 {
     int array[], array1[];
